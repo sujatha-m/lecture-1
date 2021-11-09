e229e3cee48f60d77350a77ee422bf161a5f66f8
sujatha-m <jhanavi.bhushan@gmail.com>
2021-11-09 14:16:28 -0500

    The function prints Name

---
+++ printName.js
@@ -0,0 +1,12 @@
+
+
+const username = process.argv[2]
+// console.log(username)
+
+function printName(){
+    console.log("welcome to Web Dev core " + username + '!')
+}
+printName()
+
+//in terminal you will run like this the name is 2nd arguemnet
+//node printName.js Sujatha
